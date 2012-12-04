57f41e0ab8a73f034d0224bb8ae96ec71d2d41b2
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2012-12-04 19:24:18 +0100

    ...

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ HTTP_PORT=9000
 export OSG_WINDOW="100 100 800 600"
 cd /Users/stephan/Documents/osg/build/bin
 ./present3Dd \
+  --device 0.0.0.0:${OSC_PORT}.receiver.osc \
   --device 0.0.0.0:${HTTP_PORT}${ROOT}/gsc_ipad.resthttp \
   --device _p3d_http._tcp:${HTTP_PORT}.advertise.zeroconf \
   --device _p3d_osc._udp:${OSC_PORT}.advertise.zeroconf \
