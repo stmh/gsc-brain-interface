8a8e665a027f5534a73edf3740daad46e7c9cd20
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2012-12-07 17:39:33 +0100

    neue prese

---
+++ GSC Brain Interface/gsc brain interface osx/main.mm
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gsc brain interface osx
+//
+//  Created by Stephan Huber on 07.12.12.
+//  Copyright (c) 2012 OpenSceneGraph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ cd /Users/stephan/Documents/osg/build/bin
   --device 0.0.0.0:${HTTP_PORT}${ROOT}/gsc_ipad.resthttp \
   --device _p3d_http._tcp:${HTTP_PORT}.advertise.zeroconf \
   --device _p3d_osc._udp:${OSC_PORT}.advertise.zeroconf \
-  /Users/stephan/Documents/Projekte/cefix/3rdParty/OpenSceneGraph-Data/cow.osg
+  ${ROOT}/gsc_ipad/interface.p3d
