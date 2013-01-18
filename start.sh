18a272f09cdc5659651c816b9193c36a2d4cad7c
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-01-18 18:54:08 +0100

    added osgPresentation's timeout functionality

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ OSC_PORT=8000
 HTTP_PORT=9000
 
 export OSG_WINDOW="100 100 800 600"
-cd /Users/stephan/Documents/osg/build/bin
+cd ../osg/build/bin
 ./present3Dd \
   --device 0.0.0.0:${OSC_PORT}.receiver.osc \
   --device 0.0.0.0:${HTTP_PORT}${ROOT}/gsc_ipad.resthttp \
   --device _p3d_http._tcp:${HTTP_PORT}.advertise.zeroconf \
   --device _p3d_osc._udp:${OSC_PORT}.advertise.zeroconf \
-  ${ROOT}/gsc_ipad/interface.p3d
+  ${ROOT}/../data/gsc_brain/brain.p3d
