25fb6fcb974d8139b8cbdb281f62340e2dc96a39
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2013-08-29 09:45:13 +0200

    changed build

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ OSC_PORT=8000
 HTTP_PORT=9000
 
 export OSG_WINDOW="100 100 800 600"
-cd ../osg/build/bin
+#export OSG_NOTIFY_LEVEL=INFO
+cd ../osg_osx/bin
 ./present3Dd \
   --device 0.0.0.0:${OSC_PORT}.receiver.osc \
   --device 0.0.0.0:${HTTP_PORT}${ROOT}/gsc_ipad.resthttp \
